49b023ba41332ea4fc765640981da377a0f47d34
tengis4262 <e.tengis@gmail.com>
2023-04-12 11:48:48 -0500

    first commit
    first commit

---
+++ Assignment/Assignment1/objectDiagram.js
@@ -0,0 +1,2 @@
+class Animal { 
+    constructor(name) { this.speed = 0; this.name = name; } run(speed) { this.speed = speed; alert(`${this.name} runs with speed ${this.speed}.`); } stop() { this.speed = 0; alert(`${this.name} stands still.`); } } let animal = new Animal("My animal"); class Rabbit extends Animal { hide() { alert(`${this.name} hides!`); } } let rabbit = new Rabbit("White Rabbit"); rabbit.run(5); // White Rabbit runs with speed 5.rabbit.hide(); // White Rabbit hides!  
